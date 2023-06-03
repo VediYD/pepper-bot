d218a1a157ce92afbc5989f246709e6936512c42
Cheeky <sudaksh77@gmail.com>
2023-06-03 17:22:53 +1000

    Wrote down 2 methods to test with pepper.

--- flask_server.py
+++ flask_server.py
@@ -1,16 +1,26 @@
 from flask import Flask, request
 from naoqi import ALProxy
+import time
 
 app = Flask(__name__)
-speech_proxy = ALProxy("ALTextToSpeech", "192.168.0.192", 9559)
+# Bot's IP address
+ip = "192.168.0.192"
+port = 9559
+speech_proxy = ALProxy("ALTextToSpeech", ip, port)
+speech_recognition_proxy = ALProxy("ALSpeechRecognition", ip, port)
+memory_proxy = ALProxy("ALMemory", ip, port)
 
 
 @app.route('/say', methods=['POST'])
 def say_something():
-    #message = "Hey Samruddhi, I think it's working"
     data = request.form.to_dict()
     speech_proxy.say(str(data['message']))
     return 'OK'
+
+@app.route('/listen', methods=['POST'])
+def listen():
+    pass
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8891)
