8b61c4f8fdf768672badb75e4cb1e3f2b73de95d
Cheeky <sudaksh77@gmail.com>
2023-07-13 13:38:57 +1000

    Starting speech t text

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+!pip install speechrecognition==3.8.1
+from flask import Flask, request, jsonify
 from naoqi import ALProxy
 import time
+import speech_recognition as sr
 
 app = Flask(__name__)
 # Bot's IP address
@@ -21,6 +23,39 @@ def say_something():
 def listen():
     pass
 
+def convert_wav_to_text(audio_path):
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        audio_data = r.record(source)
+    try:
+        text = r.recognize_google(audio_data)
+        return text
+    except sr.UnknownValueError:
+        return "Speech recognition could not understand audio"
+    except sr.RequestError as e:
+        return "Could not request results from Google Speech Recognition service: " + str(e)
+    except Exception as e:
+        return "An error occurred during speech recognition: " + str(e)
+
+
+@app.route('/convert', methods=['POST'])
+def convert_audio():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
+
+    audio_file = request.files['file']
+    if audio_file.filename == '':
+        return jsonify({'error': 'No file selected'})
+
+    # Save the uploaded file
+    audio_path = 'recordings/' + audio_file.filename
+    audio_file.save(audio_path)
+
+    # Convert audio to text
+    result = convert_wav_to_text(audio_path)
+
+    # Return the converted text
+    return jsonify({'text': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8891)
