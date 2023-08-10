faa8ff68305a6679a51c2f952c96bf6cac207f71
VediYD <vedi.yash@gmail.com>
2023-08-10 20:05:18 +1000

    gpt server updated

--- main-gpt.py
+++ main-gpt.py
@@ -4,21 +4,20 @@
 from langchain.chains           import RetrievalQA
 from langchain.llms             import GPT4All
 from langchain.embeddings       import HuggingFaceEmbeddings
-from langchain.text_splitter    import RecursiveCharacterTextSplitter
 from langchain.vectorstores     import Chroma
 from langchain.prompts          import PromptTemplate
 from langchain.document_loaders import CSVLoader
 
 from pandas import read_csv
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 
 print('imports complete')
 # load course data
 # load the data using csv loader
 loader = CSVLoader(
-    './course-data-small.csv', 
+    './gpt-summarized-info.csv', 
     source_column="overview", 
     csv_args={
         "delimiter": ",", 
@@ -33,10 +32,10 @@ print('context document loaded')
 # this still has an issue where it doesnt take just the overview column and instead uses all the columns and splits them weirdly. 
 # refer the main.ipynb notebook for more information
 
-# split the documents into chunks of 256 characters each
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=64)
-texts = text_splitter.split_documents(documents)
-print('context split into chunks of size 256 and overlap 64 characters')
+# # split the documents into chunks of 256 characters each
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=64)
+# texts = text_splitter.split_documents(documents)
+# print('context split into chunks of size 256 and overlap 64 characters')
 
 # load the embeddings to store the sentence transformations and search results
 embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
@@ -45,7 +44,16 @@ print('embeddings loaded for sentence similarity search')
 
 # load the model that would be used to generate the responses
 model_path = '/app/ggml-gpt4all-j-v1.3-groovy.bin'
-llm = GPT4All(model=model_path, backend='gptj', verbose=True, n_threads=8)
+llm = GPT4All(
+    model=model_path, 
+    backend='gptj',
+    verbose=False, 
+    n_threads=8, 
+    seed=42, 
+    temp=0.3, 
+    streaming=False, 
+    use_mlock=True
+)
 print('GPT4ALL model loaded - 1.3 groovy')
 
 # create the prompt template and create the QA object
@@ -64,7 +72,7 @@ qa = RetrievalQA.from_chain_type(
     chain_type='stuff',
     retriever=db.as_retriever(),
     chain_type_kwargs={"prompt": PROMPT},
-    return_source_documents=False,
+    return_source_documents=True,
     verbose=False,
 )
 print('QA object initialized')
@@ -76,11 +84,8 @@ def handle_question():
     data = request.get_json()  # Assuming the question is sent as a JSON payload
     question = data['question']
     
-    # Call the answer_question function
-    answer = qa(question)
-    
     # Return the answer as a JSON response
-    return jsonify({'answer': answer['result']})
+    return Response(qa(question), mimetype='application/json')
 
 
 if __name__ == '__main__':
