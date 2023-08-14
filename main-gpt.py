01351248b1b8a06892dbd62dac7e188cda81df56
Cheeky <sudaksh77@gmail.com>
2023-08-14 17:48:34 +1000

    Added noise reduce flask function

--- main-gpt.py
+++ main-gpt.py
@@ -83,6 +83,43 @@ def handle_question():
     # Return the answer as a JSON response
     return jsonify(course_summary=summary)
 
+@app.route('/noise', methods=['POST'])
+def noise_reducer():
+    # Get the audio file path
+    data = request.json
+    path = str(data['audio_loc'])
+    sav_loc = str(data['save_loc'])
+    prop_decrease = float(data['prop_decrease'])
+    vol_increase = float(data['vol_increase'])
+
+    # Load Data
+    rate, data = wavfile.read(path)
+    
+    # Noise Data
+    n_rate, n_data = wavfile.read("recordings/noise.wav")
+
+    # Reduce noise
+    reduced_noise = nr.reduce_noise(y=data, 
+                                    sr=rate, 
+                                    y_noise=n_data,
+                                    prop_decrease=prop_decrease, 
+                                    n_jobs=-1)
+
+    # Increase volume (make up for the volume reduction)
+    reduced_noise = reduced_noise * vol_increase
+
+    # Convert to 16-bit data
+    reduced_noise = reduced_noise.astype(np.int16)
+
+    # Save the output
+    if sav_loc != '':
+        wavfile.write(sav_loc, rate, reduced_noise)
+    else:
+        sav_loc = "recordings/rn_recording.wav"
+        wavfile.write(sav_loc, rate, reduced_noise)
+
+    return 'Saved noise reduced audio file to ' + sav_loc
+
 
 if __name__ == '__main__':
     print('Spinning Servers')
